664e1acb6ae5dca823f0dfcac830477bc7c725b9
Eugen <eugen252009@gmail.com>
2025-01-04 14:32:35 +0100

    deleted unused Files

--- metadata/metadata.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Grepolis Defense Optimizer
-// @namespace    https://grepolis.com
-// @version      0.0.2
-// @description  Zeigt an, wie stark deine Verteidigung von der optimalen Abwehr abweicht.
-// @author       Eugen252009
-// @match        https://*.grepolis.com/game/*
-// @grant        none
-// ==/UserScript==
